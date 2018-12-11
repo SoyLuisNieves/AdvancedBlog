00fe49cce5a0ba551b6a0c9e32aabb2308231f5d
Luis Fernando Nieves Nieves <luis.fernan.nieves@gmail.com>
2018-12-11 13:37:43 -0600

    Basic Customize admin

--- src/posts/admin.py
+++ src/posts/admin.py
@@ -4,4 +4,13 @@ from posts.models import Post
 
 # Register your models here.
 
-admin.site.register(Post)
+class PostAdmin(admin.ModelAdmin):
+	list_display = ["title", "timestamp", "updated"]
+	list_display_links = ["timestamp"]
+	list_editable = ["title"]
+	list_filter = ["timestamp", "updated"]
+	search_fields = ["title", "content"]
+	class Meta:
+		model = Post
+
+admin.site.register(Post, PostAdmin)
